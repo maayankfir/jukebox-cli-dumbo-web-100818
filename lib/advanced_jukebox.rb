342db4da7dcfdfa477a3b9b2ebfc953a0f42d1d7
Maayan Kfir <mayankfir@gmail.com>
2018-09-29 18:25:06 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -14,13 +14,12 @@
 
 def help
   #this method should be the same as in jukebox.rb
-  help =  "I accept the following commands:
-- help : displays this help message
-- list : displays a list of songs you can play
-- play : lets you choose a song to play
-- exit : exits this program"
+  puts  "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 
-puts help
 end
 
 
